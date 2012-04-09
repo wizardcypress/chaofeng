0b533f6fc13635732068c247ecbeb6fc18945219
Norman Mo <LTaoist6@gmail.com>
2012-04-09 17:07:38 +0800

    增加了setup.pu

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python2
+
+'''
+A simple telnet bbs server framework for python.
+'''
+
+from distutils.core import setup
+
+setup(name='chaofeng',
+      version='0.9',
+      description='A simple telnet bbs server framework for python.',
+      author='LTaoist',
+      url='https://github.com/LTaoist/chaofeng',
+      packages=['chaofeng']
+      )
