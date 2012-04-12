7162cdedd4856ad146c30ab2c3f101aca5ace5aa
Norman Mo <LTaoist6@gmail.com>
2012-04-12 09:18:01 +0800

    增加了g模块的mark

--- chaofeng/g.py
+++ chaofeng/g.py
@@ -1,9 +1,11 @@
 from chaofeng.ascii import *
 
+STATIC_PATH = './static'
+
 def load_static():
     from os import walk as os_walk
     '''Load the file under static path as string.'''
-    path='./static/'
+    path=STATIC_PATH
     v = {}
     for root,dirs,files in os_walk(path):
         for filename in files :
@@ -12,3 +14,12 @@ def load_static():
     return v
 
 static = load_static()
+
+marks = {}
+
+def mark(name):
+    def mark_inner(frame):
+        global marks
+        mraks[name] = frame
+        return frame
+    return mark_inner
